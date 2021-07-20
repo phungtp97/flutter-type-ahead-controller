c42f76dcf22851c772f50711ee3fd35372d32edd
phungtp97@gmail.com <phungtp97@gmail.com>
2021-07-20 20:31:15 +0700

    remove files

--- android/app/src/main/kotlin/com/typeahead/flutter_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.typeahead.flutter_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/typeahead/phungta/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.typeahead.phungta
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
